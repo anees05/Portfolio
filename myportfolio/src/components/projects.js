d31c0ee50fe5b890d32c57119be899e6b735cdcf
anees05 <anees.adams05@gmail.com>
2019-09-16 16:14:46 -0400

    Create card templates for projects and style those cards

--- myportfolio/src/components/projects.js
+++ myportfolio/src/components/projects.js
@@ -1,11 +1,88 @@
 import React, { Component } from "react";
-import { Tabs, Tab } from "react-mdl";
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from "react-mdl";
 
 class Projects extends Component {
     constructor(props) {
         super(props);
         this.state = { activeTab: 0 };
     }
+
+    toggleCategories() {
+        if (this.state.activeTab === 0) {
+            return (
+                <div className="projects-grid">
+                <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+                    <CardTitle style={{ color: "#fff", height: "176px", background: "url() center / cover" }}>React Project #1</CardTitle>
+                    <CardText>
+                        Lorem Ipsum
+                        </CardText>
+                    <CardActions border>
+                        <Button colored style={{width: "100%"}}>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{ color: "#fff" }}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+
+                <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+                    <CardTitle style={{ color: "#fff", height: "176px", background: "url() center / cover" }}>React Project #1</CardTitle>
+                    <CardText>
+                        Lorem Ipsum
+                        </CardText>
+                    <CardActions border>
+                        <Button colored style={{width: "100%"}}>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{ color: "#fff" }}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                </Card>
+                </div>
+            )
+        } else if (this.state.activeTab === 1) {
+            return (
+                <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+                    <CardTitle style={{ color: "#fff", height: "176px", background: "url() center / cover" }}></CardTitle>
+                </Card>
+            )
+        } else if (this.state.activeTab === 2) {
+            return (
+                <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+                    <CardTitle style={{ color: "#fff", height: "176px", background: "url() center / cover" }}></CardTitle>
+                </Card>
+            )
+        } else if (this.state.activeTab === 3) {
+            return (
+                <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+                    <CardTitle style={{ color: "#fff", height: "176px", background: "url() center / cover" }}></CardTitle>
+                </Card>
+            )
+        } else if (this.state.activeTab === 4) {
+            return (
+                <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+                    <CardTitle style={{ color: "#fff", height: "176px", background: "url() center / cover" }}></CardTitle>
+                </Card>
+            )
+        } else if (this.state.activeTab === 5) {
+            return (
+                <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+                    <CardTitle style={{ color: "#fff", height: "176px", background: "url() center / cover" }}></CardTitle>
+                </Card>
+            )
+        } else if (this.state.activeTab === 6) {
+            return (
+                <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+                    <CardTitle style={{ color: "#fff", height: "176px", background: "url() center / cover" }}></CardTitle>
+                </Card>
+            )
+        } else if (this.state.activeTab === 7) {
+            return (
+                <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+                    <CardTitle style={{ color: "#fff", height: "176px", background: "url() center / cover" }}></CardTitle>
+                </Card>
+            )
+        }
+    }
+
     render() {
         return (
             <div className="category">
@@ -13,15 +90,21 @@ class Projects extends Component {
                     <Tab>React</Tab>
                     <Tab>Javascript</Tab>
                     <Tab>Jquery</Tab>
+                    <Tab>NodeJS</Tab>
                     <Tab>MySQL</Tab>
                     <Tab>MongoDB</Tab>
                     <Tab>HandleBars</Tab>
                     <Tab>Projects</Tab>
                 </Tabs>
 
-                <section className="projects-grid">
-                {this.toggleCategories()}
-                </section>
+                
+                    <Grid>
+                        <Cell col={12}>
+                            <div className="content">
+                                {this.toggleCategories()}
+                            </div>
+                        </Cell>
+                    </Grid>
             </div>
         )
     }
