57e8e1672ae200ae0d82d0858ee12323af4058ce
anees05 <anees.adams05@gmail.com>
2019-09-17 10:07:03 -0400

    Add home page link

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Layout>
         <Header className="header-color" title="Anees Adams" scroll>
           <Navigation>
+            <Link to="/">Home</Link>
             <Link to="/Resume">Resume</Link>
             <Link to="/About">About</Link>
             <Link to="/Projects">Projects</Link>
