364ad02161dfe4e62021a15944d165beadaebb89
anees05 <anees.adams05@gmail.com>
2019-09-17 10:56:50 -0400

    Add image and text to contact page

--- myportfolio/src/components/contact.js
+++ myportfolio/src/components/contact.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import Image from "./IMG_3650.png"
+
 
 class Contact extends Component {
     render() {
         return (
-            <div>
-                <h1>
-                    Contact Page
-                </h1>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Anees Adams</h2>
+                        <img
+                            src={Image}
+                            alt="anees"
+                            style={{ height: "250px" }}
+                        />
+                        <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>Lorem Ipsum</p>
+                    </Cell>
+                    <Cell col={6}>
+                        
+                    </Cell>
+                </Grid>
+
             </div>
         )
     }
