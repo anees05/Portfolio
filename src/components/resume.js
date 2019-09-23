3cc45da275db9a3f49f6aa672c5e9cd27840501c
anees05 <anees.adams05@gmail.com>
2019-09-23 11:11:01 -0400

    Fix typos in resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -12,21 +12,21 @@ class Resume extends Component {
                     <Cell col={4}>
                         <div>
                             <h2>Anees Adams</h2>
-                            <h4 style={{ color: "grey" }}> Full Stack Web Developer</h4>
+                            <h4 style={{ color: "grey" }}> Full-Stack Web Developer</h4>
                             <hr style={{ borderTop: "3px solid #45a247", width: "50%" }} />
-                            <p>I am a front-end web developer with a background in exercise science.
+                            <p>I am a Full-Stack web developer with a background in exercise science.
                                 I earned a certificate in Full Stack Web Development from University of North Carolina at Charlotte.
                                 I use HTML5, CSS3, JavaScript, jQuery, Node.js, MySQL, Sequelize, MongoDB, Mongoose, and Git.
                                 I love UX-Design, logical thinking, and work well with people.</p>
                             <hr style={{ borderTop: "3px solid #45a247", width: "50%" }} />
-                            <h5>Address</h5>
-                            <p>9700 Commons East Drive Charlotte, NC</p>
+                            <h5>Location</h5>
+                            <p>Charlotte, NC</p>
                             <h5>Phone</h5>
                             <p>(919) 455-0338</p>
                             <h5>Email</h5>
                             <p>anees.adams05@gmail.com</p>
                             <h5>Website</h5>
-                            <p>aneesadams.dev</p>
+                            <p>aneesadams.com</p>
                             <hr style={{ borderTop: "3px solid #45a247", width: "50%" }} />
                         </div>
                     </Cell>
@@ -35,7 +35,7 @@ class Resume extends Component {
                         <Education
                             date={"June 2019 - September 2019"}
                             schoolName="University of North Carolina at Charlotte"
-                            schoolDescription="Full Stack Web Developer Bootcamp"
+                            schoolDescription="Full-Stack Web Developer Bootcamp"
                         />
                         <hr style={{ borderTop: "3px solid #45a247" }} />
 
