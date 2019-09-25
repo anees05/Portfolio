0d1039e7de9f33440965a5fa07ca8b7aeba86b0b
anees05 <anees.adams05@gmail.com>
2019-09-25 15:32:28 -0400

    Put projects in grid

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,58 +11,69 @@ class Projects extends Component {
         if (this.state.activeTab === 0) {
             return (
                 <div className="projects-grid">
-                    <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-                        <Button colored style={{ width: "100%", height: "100%" }}>
-                            <a href="https://anees05.github.io/Project-1/" rel="noopener noreferrer" target="blank">
-                                <CardTitle style={{ color: "black", height: "176px", background: "url(https://snipboard.io/1TFmaG.jpg) center / cover" }}>Project #1
+                    <Grid>
+                        <Cell col={4}>
+                            <Card shadow={5} style={{ minWidth: "450", margin: "auto" }} className="project-1">
+                                <Button colored style={{ width: "100%", height: "100%" }}>
+                                    <a href="https://anees05.github.io/Project-1/" rel="noopener noreferrer" target="blank">
+                                        <CardTitle style={{ color: "black", height: "176px", background: "url(https://snipboard.io/1TFmaG.jpg) center / cover" }}>Project #1
                             </CardTitle>
-                            </a>
-                        </Button>
-                        <CardText>
-                            Event Hunters is an event styled app that allows users to search for upcoming events in their area and is designed to help make quick and easy decisions for finding the right pastime.                        </CardText>
-                        <CardActions border>
-                            <Button colored style={{ width: "100%" }}><a href="https://github.com/anees05/Project-1" rel="noopener noreferrer" target="blank">Github</a></Button>
-                        </CardActions>
-                        <CardMenu style={{ color: "#fff" }}>
-                        </CardMenu>
-                    </Card>
+                                    </a>
+                                </Button>
+                                <CardText>
+                                    Event Hunters is an event styled app that allows users to search for upcoming events in their area and is designed to help make quick and easy decisions for finding the right pastime.
+                                    </CardText>
+                                <CardActions border>
+                                    <Button colored style={{ width: "100%" }}><a href="https://github.com/anees05/Project-1" rel="noopener noreferrer" target="blank">Github</a></Button>
+                                </CardActions>
+                                <CardMenu style={{ color: "#fff" }}>
+                                </CardMenu>
+                            </Card>
+                        </Cell>
+                    </Grid>
 
-                    <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-                        <Button colored style={{ width: "100%", height: "100%" }}>
-                            <a href="https://jobs-r-us.herokuapp.com/" rel="noopener noreferrer" target="blank">
-                                <CardTitle style={{ color: "black", height: "176px", background: "url(https://snipboard.io/IFKXGx.jpg) center / cover" }}>Project #2
+                    <Grid>
+                        <Cell col={4}>
+                            <Card shadow={5} style={{ minWidth: "450", margin: "auto" }} className="project-2">
+                                <Button colored style={{ width: "100%", height: "100%" }}>
+                                    <a href="https://jobs-r-us.herokuapp.com/" rel="noopener noreferrer" target="blank">
+                                        <CardTitle style={{ color: "black", height: "176px", background: "url(https://snipboard.io/IFKXGx.jpg) center / cover" }}>Project #2
                             </CardTitle>
-                            </a>
-                        </Button>
-                        <CardText>
-                            Jobs-R-Us is a web app that allows users to make searches for job information.
+                                    </a>
+                                </Button>
+                                <CardText>
+                                    Jobs-R-Us is a web app that allows users to make searches for job information.
                         </CardText>
-                        <CardActions border>
-                            <Button colored style={{ width: "100%" }}><a href="https://github.com/anees05/JobStuff2" rel="noopener noreferrer" target="blank">Github</a></Button>
-                        </CardActions>
-                        <CardMenu style={{ color: "#fff" }}>
-                        </CardMenu>
-                    </Card>
+                                <CardActions border>
+                                    <Button colored style={{ width: "100%" }}><a href="https://github.com/anees05/JobStuff2" rel="noopener noreferrer" target="blank">Github</a></Button>
+                                </CardActions>
+                                <CardMenu style={{ color: "#fff" }}>
+                                </CardMenu>
+                            </Card>
+                        </Cell>
+                    </Grid>
 
-                    <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-                        <Button colored style={{ width: "100%", height: "100%" }}>
-                            <a href="https://glacial-savannah-65289.herokuapp.com/" rel="noopener noreferrer" target="blank">
-                                <CardTitle style={{ color: "black", height: "176px", background: "url(https://snipboard.io/kauSd3.jpg) center / cover" }}>Project #3
+                    <Grid>
+                        <Cell col={4}>
+                            <Card shadow={5} style={{ minWidth: "450", margin: "auto" }} className="project-3">
+                                <Button colored style={{ width: "100%", height: "100%" }}>
+                                    <a href="https://glacial-savannah-65289.herokuapp.com/" rel="noopener noreferrer" target="blank">
+                                        <CardTitle style={{ color: "black", height: "176px", background: "url(https://snipboard.io/kauSd3.jpg) center / cover" }}>Project #3
                             </CardTitle>
-                            </a>
-                        </Button>
-                        <CardText>
-                            RoadTripDJ creates a Spotify playlist for your road trip with musicians from the cities you’re traveling to and from. Discover new music, find local artists, and keep your long drives interesting with RoadTripDJ.
+                                    </a>
+                                </Button>
+                                <CardText>
+                                    RoadTripDJ creates a Spotify playlist for your road trip with musicians from the cities you’re traveling to and from. Discover new music, find local artists, and keep your long drives interesting with RoadTripDJ.
                         </CardText>
-                        <CardActions border>
-                            <Button colored style={{ width: "100%" }}><a href="https://github.com/Tim1986/RoadTripDJ" rel="noopener noreferrer" target="blank">Github</a></Button>
-                        </CardActions>
-                        <CardMenu style={{ color: "#fff" }}>
-                        </CardMenu>
-                    </Card>
-
-
-                </div>
+                                <CardActions border>
+                                    <Button colored style={{ width: "100%" }}><a href="https://github.com/Tim1986/RoadTripDJ" rel="noopener noreferrer" target="blank">Github</a></Button>
+                                </CardActions>
+                                <CardMenu style={{ color: "#fff" }}>
+                                </CardMenu>
+                            </Card>
+                        </Cell>
+                    </Grid >
+                </div >
             )
             // } else if (this.state.activeTab === 1) {
             //     return (
